79077e6efd00682b3f044257d8d067e2e9bc2b2e
Yusuke Sangenya <longinus.eva@gmail.com>
2018-10-13 16:35:37 +0900

    view

--- lib/view.rb
+++ lib/view.rb
@@ -2,12 +2,13 @@ require 'nokogiri'
 
 module View
   class Entry
-    attr_reader :entry_url, :title
+    attr_reader :entry_url, :title, :published_at
 
-    def initialize(entry_url, title, abstract_html)
+    def initialize(entry_url, title, abstract_html, published_at)
       @entry_url = entry_url
       @title = title
       @abstract_html = abstract_html
+      @published_at = published_at
     end
 
     def abstract
--- server.rb
+++ server.rb
@@ -5,8 +5,8 @@ require 'table'
 require 'view'
 
 get '/' do
-  @entries = Table::Entry.order(:published_at).map do |e|
-    View::Entry.new(e.entry_url, e.title, e.abstract)
+  @entries = Table::Entry.order(Sequel.desc(:published_at)).map do |e|
+    View::Entry.new(e.entry_url, e.title, e.abstract, e.published_at)
   end
 
   haml :index
